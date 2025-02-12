7eb64cb78737cb0156b65b066662a6ca090002a1
YasuTasu <yasuhiro.tanaka.2021@globis.ac.jp>
2025-02-12 16:59:41 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,22 +1,22 @@
 from fastapi import FastAPI
-from app.routes import product, sale
 from fastapi.middleware.cors import CORSMiddleware
+from app.routes import product, sale
 
 app = FastAPI()
 
-# ルーター登録
-app.include_router(product.router)
-app.include_router(sale.router)
-
-# 本番環境では特定のフロントエンドのみ許可
+# ここでフロントエンドのURLを指定
 origins = [
-    "https://tech0-gen8-step4-pos-app-79.azurewebsites.net"
+    "https://tech0-gen8-step4-pos-app-79.azurewebsites.net"  # フロントエンドURL
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,  # 許可するオリジンを設定
+    allow_origins=origins,  # 許可するオリジンを制限
     allow_credentials=True,
-    allow_methods=["*"],  # 全てのHTTPメソッド（GET, POST, PUT, DELETEなど）を許可
-    allow_headers=["*"],  # 全てのヘッダーを許可
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
+
+# ルーター登録
+app.include_router(product.router)
+app.include_router(sale.router)
